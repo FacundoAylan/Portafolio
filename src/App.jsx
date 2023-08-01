8ecd8b34f754e13d859570cb1b184ba7abe39c55
Facundo <facundogsb@gmail.com>
2023-07-31 22:38:55 -0300

    agregando carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './Component/1-NavBar/NavBar';
 import HomePage from './Component/2-HomePage/HomePage';
 import About from './Component/3-About/About';
+import Skills from './Component/4-Skills/Skills';
 import Project from './Component/5-Projects/Projects';
-import Contact from './Component/4-Contact/Contact';
+import Contact from './Component/6-Contact/Contact';
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/skills" element={<Skills/>}/>
           <Route path="/project" element={<Project/>} />
           <Route path="/contact" element={<Contact/>} />
         </Routes>
---
+++ src/Component/4-Skills/Skills.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <h1>Skill</h1>
+  )
+};
+
+export default Skills;
