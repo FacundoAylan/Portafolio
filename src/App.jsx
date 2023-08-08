356cc8c32fedaf8cadc9ae54eccd82b14fc2b9a8
Facundo <facundogsb@gmail.com>
2023-08-07 23:22:17 -0300

    mejoras en el componente home

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import NavBar from './Component/1-NavBar/NavBar';
 import HomePage from './Component/2-HomePage/HomePage';
 import Project from './Component/3-Projects/Projects';
 import Contact from './Component/4-Contact/Contact';
+import './index.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='container-app'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
