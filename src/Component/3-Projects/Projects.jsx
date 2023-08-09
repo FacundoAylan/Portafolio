d4ecebb5dd9146355e924a0df73a57f27555602a
Facundo <facundogsb@gmail.com>
2023-08-09 01:26:34 -0300

    mejoras en el componente projects

--- src/Component/3-Projects/Projects.jsx
+++ src/Component/3-Projects/Projects.jsx
@@ -9,23 +9,49 @@ import image3 from '../../assets/home.jpg';
 const images = [image1,image2,image3];
 
 const Projects = () => {
-  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [currentImage, setCurrentImage] = useState(0);
 
   const handleImageChange = (index) => {
-    setCurrentImage(images[index]);
+    setCurrentImage(index);
+  };
+  const handlePrevImage = () => {
+    const newIndex = (currentImage - 1 + images.length) % images.length;
+    setCurrentImage(newIndex);
+  };
+
+  const handleNextImage = () => {
+    const newIndex = (currentImage + 1) % images.length;
+    setCurrentImage(newIndex);
   };
 
   return (
     <div className="carousel-container">
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${currentImage})` }}
+        style={{ backgroundImage: `url(${images[currentImage]})` }}
       >
-        <h1>titulo del projecto</h1>
-        <h2>description del projecto</h2>
-        <h2>aportes</h2>
-        <h2>tegnologias</h2>
+        <div className='container-info'>
+          <h1>titulo del projecto</h1>
+          <h2>description del projecto</h2>
+          <h2>aportes</h2>
+          <h2>tegnologias</h2>
+        </div>
       </div>
+      <h1 className='title'>Projects</h1>
+      <button
+        className='next'
+        onClick={handleNextImage}
+        disabled={currentImage === 0}
+      >
+        &#8249;
+      </button>
+      <button
+        className='prev'
+        onClick={handlePrevImage}
+        disabled={currentImage === images.length - 1}
+      >
+        &#8250;
+      </button>
       <Carousel
         showArrows={true}
         onChange={handleImageChange}
@@ -33,8 +59,8 @@ const Projects = () => {
         className="carousel"
       >
         {images.map((image, index) => (
-          <div key={index} className='container-image'>
-            <img  className='image' src={image} alt={`Image ${index}`} />
+          <div className='container-image' key={index}>
+            <img src={image} alt={`Image ${index}`} />
           </div>
         ))}
       </Carousel>
