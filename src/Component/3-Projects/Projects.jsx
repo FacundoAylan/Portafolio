6e5b228cfedfb5627b5cd4b647f18f1347726219
Nahuel <lescanoagustin10nahuel@gmail.com>
2024-03-02 01:57:41 -0300

    Animaciones en la home

--- src/Component/3-Projects/Projects.jsx
+++ src/Component/3-Projects/Projects.jsx
@@ -1,52 +1,73 @@
 import { slides } from './data';
-import { Box, Flex, Text } from '@chakra-ui/react';
-import background from './image/background.png'
+import { useRef } from "react";
+import { Box, Flex, Text, ScaleFade } from '@chakra-ui/react';
+import { useInViewport } from "react-in-viewport";
 import Cardprojects from './card';
+import background from './image/background.png'
 
 const Projects = () => {
-  return (
-    <Box 
-      width='100%' 
-      minHeight='100vh' 
-      backgroundImage={background}
-      backgroundSize='100% 100%'
-      backgroundRepeat='no-repeat'
-      backgroundPosition='center'
-      paddingTop='2%'
-      paddingBottom='2%'
-    >
-      <Text 
-        fontSize='2rem' 
-        textTransform='uppercase'
-        color='white'
-        fontWeight='bold'
-        textAlign='center'
-      >
-        Proyectos
-      </Text>
-      <Flex 
-        width='100%' 
-        height='auto'
-        flexDirection='row' 
-        flexWrap='wrap'
-        justifyContent='center'
-        gap={{base:3, lg:8}}
-        marginTop='2%'
-      >
-        {slides.map((slide, index) => {
-          return (
-            <Cardprojects
-              key={index}
-              image={slide.image} 
-              title={slide.title} 
-              subtitle={slide.subtitle}
-              rigth={slide.rightSubtitle}
-            />
-          )
-        })}
-      </Flex>
-    </Box>
-  );
+    const ref = useRef(null);
+
+    const { inViewport } = useInViewport(
+        ref,
+        { rootMargin: "-100px" },
+        { disconnectOnLeave: false },
+        {}
+    );
+
+    return (
+        <Box 
+            width='100%' 
+            minHeight='100vh' 
+            backgroundImage={background}
+            backgroundSize='100% 100%'
+            backgroundRepeat='no-repeat'
+            backgroundPosition='center'
+            paddingTop='2%'
+            paddingBottom='2%'
+        >
+            <Text 
+                fontSize='2rem' 
+                textTransform='uppercase'
+                color='white'
+                fontWeight='bold'
+                textAlign='center'
+            >
+                Proyectos
+            </Text>
+            <Flex
+                width='100%' 
+                height='auto'
+                flexDirection='row' 
+                flexWrap='wrap'
+                justifyContent='center'
+                gap={{base: 3, lg: 8}}
+                marginTop='2%'
+            >
+                    {slides.map((slide, index) => {
+                        return (
+                            <Box
+                                width={{ base: '45%', lg: '20%' }}
+                                height={{ base: '40vh', sm: '50vh', lg: '40vh' }}
+                                key={index}
+                                flexDirection="row"
+                                ref={ref}
+                            >
+                            <ScaleFade initialScale={0.9} in={inViewport} whileHover={{ scale: 1.1 }}>
+                            <Cardprojects
+                                image={slide.image}
+                                title={slide.title}
+                                subtitle={slide.subtitle}
+                                rigth={slide.rightSubtitle}
+                            />
+                            </ScaleFade>
+                            </Box>
+                        )
+                    })}
+
+            </Flex>
+        </Box>
+    );
 };
 
 export default Projects;
--- src/Component/3-Projects/card.jsx
+++ src/Component/3-Projects/card.jsx
@@ -1,20 +1,10 @@
-import React, { useRef, useState } from 'react';
-import { Box, Text, Image, Modal, ModalOverlay, ModalContent, ModalBody, IconButton, Button, ScaleFade } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Text, Image, Modal, ModalOverlay, ModalContent, ModalBody, IconButton, Button } from '@chakra-ui/react';
 import { FaPlay } from 'react-icons/fa'; 
-import { useInViewport } from "react-in-viewport";
 
 const Cardprojects = ({ id, image, title, subtitle, right }) => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const ref = useRef(null);
-
-    const { inViewport } = useInViewport(
-        ref,
-        { rootMargin: "-200px" },
-        { disconnectOnLeave: false },
-        {}
-    );
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -26,8 +16,8 @@ const Cardprojects = ({ id, image, title, subtitle, right }) => {
 
     return (
         <Box
-            width={{ base: '45%', lg: '20%' }}
-            height={{ base: '40vh', sm: '50vh', lg: '40vh' }}
+            h="100%"
+            w="100%"
             backgroundColor='#0b5c92'
             border='2px double white'
             borderRadius='12px'
@@ -38,9 +28,7 @@ const Cardprojects = ({ id, image, title, subtitle, right }) => {
             _hover={{ border: '4px solid #ea63fe' }}
             onClick={openModal}
             position='relative'
-            ref={ref}
         >
-            <ScaleFade initialScale={0.9} in={inViewport} whileHover={{ scale: 1.1 }}>
                 <IconButton
                     icon={<FaPlay />}
                     onClick={openModal}
@@ -59,10 +47,10 @@ const Cardprojects = ({ id, image, title, subtitle, right }) => {
                 <Image
                     src={image}
                     width='100%'
-                    height='60%'
+                    height='25vh'
                     borderBottom='1px solid white'
-                    className='animation'
                     key={id}
+                    overflow="hidden"
                 />
                 <Text fontSize={{ base: '1rem', lg: '1.5rem' }}>{title}</Text>
                 <Text>{subtitle}</Text>
@@ -86,7 +74,6 @@ const Cardprojects = ({ id, image, title, subtitle, right }) => {
                         </ModalBody>
                     </ModalContent>
                 </Modal>
-            </ScaleFade>
         </Box>
     )
 }
