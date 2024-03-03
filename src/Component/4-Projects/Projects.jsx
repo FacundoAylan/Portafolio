60177e66bd952ad478346af89b8ad40afc462135
FacundoAylan <facundogsb@gmail.com>
2024-03-03 00:55:17 -0300

    subiendo nuevas animaciones

--- src/Component/4-Projects/Projects.jsx
+++ src/Component/4-Projects/Projects.jsx
@@ -10,6 +10,15 @@ const animation = keyframes`
     transform:translateX(0)
   }
 `
+const animation2 = keyframes`
+  from{
+    transform:translateY(-200%)
+  }
+  to{
+    transform:translateY(0)
+  }
+`
+
 const Projects = () => {
   return (
     <Box width="100%" minHeight="100vh" paddingTop="1%" paddingBottom="2%">
@@ -19,6 +28,7 @@ const Projects = () => {
         color="white"
         fontWeight="bold"
         textAlign="center"
+        animation={`${animation2} 1.5s ease`}
       >
         Proyectos
       </Text>
