cf09ad1a6e5aa5438b1cf5502deffa59acfc3add
FacundoAylan <facundogsb@gmail.com>
2024-04-25 23:07:34 -0300

    subiendo nuevos cambios de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
       position="relative"
       padding="0"
       margin="0"
-      backgroundImage="url('https://firebasestorage.googleapis.com/v0/b/padel-1d48a.appspot.com/o/principal.jpg?alt=media&token=0f355005-3bc1-4fac-9de5-64280a4c313f')"
+      backgroundImage={principal}
       backgroundSize={{ base: '250% 100%', sm: '350% 100%', lg: '100% 100%' }}
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
